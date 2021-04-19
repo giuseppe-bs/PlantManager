954accab51a76ad1dcf24e6ad9a68197318643b5
Giuseppe B. S <giuseppescassiotti@hotmail.com>
2021-04-19 09:50:43 -0300

    Initial commit

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
+
+import wateringImg from  '../assets/watering.png';
+
+export function Welcome(){
+    return(
+        <View>
+            <Text>
+                hi
+            </Text>
+        </View>
+    )
+}
